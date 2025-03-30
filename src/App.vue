fec35513d405ef54b39672557f3c9f8e4927a271
Rafael Vicente <rafaelpv15@gmail.com>
2025-03-30 13:11:35 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Olá mundo')
+</script>
+
+<style lang="scss" scoped></style>
