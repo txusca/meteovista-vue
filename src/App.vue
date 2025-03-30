255cf821f77b34c10541e4cdf65823164c529a49
Rafael Vicente <rafaelpv15@gmail.com>
2025-03-30 13:22:41 -0300

    taiwind configuration

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    {{ message }}
+
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 
-const message = ref('Olá mundo')
 </script>
-
-<style lang="scss" scoped></style>
